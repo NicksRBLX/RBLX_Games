03b9520db1125346b734228650cc2f15a9f44a59
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-04-20 03:44:51 +0800

    Moved & Update

--- Games.lua
+++ Games.lua
@@ -1,7 +1,7 @@
 return {
 	["UniverseIDs"] = {
-		[2239430935] = "https://raw.githubusercontent.com/NicksRBLX/RBLX_Games/refs/heads/master/Blair.lua"; -- Blair
-    	[7703614594] = "https://raw.githubusercontent.com/NicksRBLX/RBLX_Games/refs/heads/master/GardenTowerDefense.lua"; -- Garden Tower Defense
+		[2239430935] = "https://raw.githubusercontent.com/NicksRBLX/RBLX_Games/refs/heads/master/Games/Blair.lua"; -- Blair
+    	[7703614594] = "https://raw.githubusercontent.com/NicksRBLX/RBLX_Games/refs/heads/master/Games/GardenTowerDefense.lua"; -- Garden Tower Defense
 	};
 	["PlaceIDs"] = {
 	};
