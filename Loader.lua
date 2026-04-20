49de6735699894457e9ce9cd65006a6e313a5d63
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-04-21 04:31:30 +0800

    Changes

--- Loader.lua
+++
@@ -1,8 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/NicksRBLX/RBLX_Games/refs/heads/master/Games.lua"))();
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/".. game.PlaceId .."/universe")).universeId;
-
-if Games["UniverseIDs"][UniverseID] then
-	loadstring(game:HttpGet(Games["UniverseIDs"][UniverseID]))();
-elseif Games["PlaceIDs"][game.PlaceId] then
-	loadstring(game:HttpGet(Games["PlaceIDs"][game.PlaceId]))();
-end
